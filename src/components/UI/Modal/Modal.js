9f812c306c2197fa56ae3c52641725c3163c61bb
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-12 18:50:45 -0700

    Changed Styles Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,6 +3,10 @@ import React from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 const Modal = (props) => {
+    let types =[]; 
+    props.types.forEach(type => {
+        types.push(type.type.name)
+    })
     return (
         <React.Fragment>
             <Backdrop show={props.show} clicked={props.clicked} />
@@ -14,31 +18,28 @@ const Modal = (props) => {
                 }}>
                 <h1 className={classes.Name}>{props.name}</h1>
                 <h3>Stats:</h3>
-                <ul>
-                    <li>
-                        Height: {props.height}
-                    </li>
-                    {props.stats.map((stat, id) => {
-                        return <li key={'stat' + id}>
-                            <span className={classes.Capitalize}>{stat.stat.name}</span>{`: ${stat.base_stat}`}
-                        </li>
-                    })}
-                    {props.types.map((type, id) => {
-                        console.log(type)
-                        return <li key={'type' + id}>
-                            {`Type: ${type.type.name}`}
-                        </li>
-                    })}
-                </ul>
-                    <div>
-                        <img src={props.front_default} height="50%" alt="Front" />
-                        <img src={props.back_default} height="25%" alt="Back" />
+                <div className="row">
+                    <div className="col-6">
+                        <ul>
+                            <li>
+                                Height: <span className={classes.Stat}>{props.height}</span>
+                            </li>
+                            {props.stats.map((stat, id) => {
+                                return <li key={'stat' + id}>
+                                    <span className={classes.Capitalize}>{stat.stat.name}</span>{':'}<span className={classes.Stat}>{stat.base_stat}</span>
+                                </li>
+                            })}
+                            <li>
+                                Type: <span className={classes.Stat}>{types.join('/')}</span>
+                            </li>
+                        </ul>
                     </div>
+                    <img src={props.front_default} height="50%" alt="Front" />
+
+                    <img src={props.back_default} height="25%" alt="Back" />
+                </div>
             </div>
         </React.Fragment>
-
-
-
     )
 }
 
